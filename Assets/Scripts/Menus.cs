787924c28fcf0ef1b69323abe358229b34329573
Jason <jason974448@gmail.com>
2026-01-18 00:19:56 -0400

    made a script to swap between scenes

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -11,6 +11,26 @@ public class Menus : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+
+        if (Input.GetKeyUp(KeyCode.Tab) && SceneManager.GetActiveScene() != SceneManager.GetSceneByBuildIndex(0))
+        {
+            ToggleScene();
+        }
+    }
+
+    void ToggleScene()
+    {
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Day"))
+        {
+            SceneManager.LoadScene("Night");
+            return;
+        }
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Night"))
+        {
+            SceneManager.LoadScene("Day");
+            return;
+        }
     }
 
     public void DayB()
